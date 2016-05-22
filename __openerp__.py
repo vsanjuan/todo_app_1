09784440da973a9e63e055567c6b5e3a377567b9
Salvador Sanjuan <salvador.sanjuan@gmail.com>
2016-05-22 12:08:37 +0200

    fix data todo_view.xml, import todo_model feat button Toggle Done

--- __openerp__.py
+++ __openerp__.py
@@ -4,5 +4,7 @@
 	'description': 'Mananage your persoal Tasks with this module',
 	'author': 'Daniel Reis',
 	'depends': ['mail'],
+	'data': ['todo_view.xml'],
 	'application': True,
+
 }
--- todo_model.py
+++ todo_model.py
@@ -1,9 +1,14 @@
 #*-* coding: utf-8 -*-
-from openerp import models, fields
+from openerp import models, fields, api
+
 class TodoTask(models.Model):
-	_name = 'todo.taks'
+	_name = 'todo.task'
 	name = fields.Char('Description', required=True)
 	is_done = fields.Boolean('Done?')
 	active = fields.Boolean('Active?', default=True)
 
-	
+	@api.one 
+	def do_toogle_done(self):
+		self.is_done =  not self.is_done
+		return True
+
