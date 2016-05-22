b163a9c5ef3656222728f7d20b59d80ec27e5730
Salvador Sanjuan <salvador.sanjuan@gmail.com>
2016-05-22 11:32:00 +0200

    feat app description

---
+++ __openerp__.py
@@ -0,0 +1,8 @@
+#*-* coding: utf-8 -*-
+{
+	'name' : 'To-Do Application',
+	'description': 'Mananage your persoal Tasks with this module',
+	'author': 'Daniel Reis',
+	'depends': ['mail'],
+	'application': True,
+}
